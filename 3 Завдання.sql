0cdd1fc234fd0179c3a0c7604a6c3b228edb7ece
Ihor Tokar <134220707+IhorTokar@users.noreply.github.com>
2024-05-24 17:25:11 +0300

    Update and rename pract4.3.sql to 3 Завдання.sql

--- 3 Завдання.sql
+++ 3 Завдання.sql
@@ -1,7 +1,7 @@
 USE CulinaryForum;
 GO
 
--- Виконання з’єднання таблиць Recipe_Ingredients та Ingredients за ingredient_id
+-- Р’РёРєРѕРЅР°РЅРЅСЏ Р·вЂ™С”РґРЅР°РЅРЅСЏ С‚Р°Р±Р»РёС†СЊ Recipe_Ingredients С‚Р° Ingredients Р·Р° ingredient_id
 SELECT *
 FROM Recipe_Ingredients
 INNER JOIN Ingredients ON Recipe_Ingredients.ingredient_id = Ingredients.ingredient_id;
