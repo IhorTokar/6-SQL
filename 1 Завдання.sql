e1c6402e1c547e277bb38b1b73aa533acf2c7f0a
Ihor Tokar <134220707+IhorTokar@users.noreply.github.com>
2024-05-24 17:24:22 +0300

    Update and rename pract4.1.sql to 1 Завдання.sql

---
+++ 1 Завдання.sql
@@ -0,0 +1,7 @@
+USE CulinaryForum;
+GO
+
+-- Вибір стовпців username і email з таблиці Users
+SELECT username, email
+FROM Users;
+GO
--- pract4.1.sql
+++
@@ -1,7 +0,0 @@
-USE CulinaryForum;
-GO
-
--- Вибір стовпців username і email з таблиці Users
-SELECT username, email
-FROM Users;
-GO
