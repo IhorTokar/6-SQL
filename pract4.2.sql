137f7b20f956c36081ad2db2076bcd32cc9f4850
Ihor Tokar <134220707+IhorTokar@users.noreply.github.com>
2024-05-24 17:24:45 +0300

    Update and rename pract4.2.sql to 2 Завдання.sql

--- pract4.2.sql
+++
@@ -1,8 +0,0 @@
-USE CulinaryForum;
-GO
-
--- Вибір користувачів, які мають роль 'User' та зареєструвались після 2023-01-01
-SELECT username, email, created_at
-FROM Users
-WHERE role_id = 2 AND created_at > '2023-01-01';
-GO
